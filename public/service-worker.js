3aa8c4fefdf0f2f3a7d10965ba43c54df04577bd
Your Name <you@example.com>
2026-04-10 23:51:45 +0800

    pwa组件

---
+++ public/service-worker.js
@@ -0,0 +1,70 @@
+// 缓存名称
+const CACHE_NAME = 'rock-fat-loss-calculator-v1';
+
+// 需要缓存的资源列表
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/body_fatrate_man.jpg',
+  '/body_fatrate_woman.jpg'
+];
+
+// 安装事件 - 缓存静态资源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('缓存已打开');
+        return cache.addAll(STATIC_ASSETS);
+      })
+  );
+});
+
+// 激活事件 - 清理旧缓存
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// fetch 事件 - 优先从缓存获取资源
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 如果在缓存中找到资源，返回缓存的资源
+        if (response) {
+          return response;
+        }
+        // 否则从网络获取
+        return fetch(event.request)
+          .then((response) => {
+            // 检查响应是否有效
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // 克隆响应，因为响应流只能使用一次
+            const responseToCache = response.clone();
+            
+            // 将新的响应添加到缓存
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          });
+      })
+  );
+});
